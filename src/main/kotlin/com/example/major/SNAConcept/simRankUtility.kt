5f05ee07ab8a0d716f1c57185e98251de53c42fb
sirius-08 <kalra.tanish8@gmail.com>
2023-04-18 02:28:45 +0530

    Added the function to get M Matrix
    Also refactored the code and started using Nd4j to make matrix
    operations easier

--- src/main/kotlin/com/example/major/SNAConcept/simRankUtility.kt
+++ src/main/kotlin/com/example/major/SNAConcept/simRankUtility.kt
@@ -1,24 +1,64 @@
 package com.example.major.SNAConcept
 
 import com.example.major.NetworkData.AuthorNetworkNode
-import jdk.internal.org.jline.utils.Colors.s
+import org.nd4j.linalg.api.ndarray.INDArray
+//import jdk.internal.org.jline.utils.Colors.s
+import org.nd4j.linalg.factory.Nd4j
 
-fun getMatrixQ(G: List<AuthorNetworkNode>): Array<DoubleArray> {
+fun getMatrixQ(G: List<AuthorNetworkNode>): INDArray{
     val stringIdToIntId: HashMap<String, Int> = HashMap<String, Int> ()
     G.forEachIndexed{ index, it ->
         stringIdToIntId.put(it.id, index)
     }
 
     val V:Int = G.size
-    var Q = Array(V) {i -> DoubleArray(V) {j -> 0.0} }
+    //var Q = Array(V) {i -> DoubleArray(V) {j -> 0.0} }
+    var Q = Nd4j.zeros(V, V)
+    var inDegree = Array(G.size) {i -> 0}
+
+    G.forEach { it ->
+        it.coAuthors.forEach { coAuthor ->
+            inDegree[stringIdToIntId[coAuthor]!!] += 1
+        }
+    }
 
     G.forEachIndexed { index, authorNetworkNode ->
         authorNetworkNode.coAuthors.forEach { it ->
             if (stringIdToIntId.containsKey(it)) {
-                Q[stringIdToIntId[it]!!][index] = 1.0 / G[stringIdToIntId[it]!!].coAuthors.size
+                //Q[stringIdToIntId[it]!!][index] = 1.0 / G[stringIdToIntId[it]!!].coAuthors.size
+                Q.put(stringIdToIntId[it]!!, index, 1.0 / inDegree[stringIdToIntId[it]!!])
             }
         }
     }
 
     return Q
 }
+
+fun getMatrixM(C: Double, src: Int, K: Int, G: List<AuthorNetworkNode>): List<List<INDArray>> {
+    val q =  Nd4j.zeros(G.size, 1)
+    q.put(src, 0, 1)
+
+    println(q)
+
+    var M: MutableList<MutableList<INDArray>> = mutableListOf()
+    var QTranspose = getMatrixQ(G).transpose()
+
+    println(QTranspose)
+
+    for(j in 0..K) {
+        M.add(mutableListOf())
+        for (i in 0 .. j)
+            M[j].add(Nd4j.zeros(G.size, 1))
+        for(i in j + 1..K + 1) {
+
+            var term1 = QTranspose.mmul(M[j][i - 1]).mul(C/2.0)
+            if(j == 0)
+                M[j].add(term1.add(q))
+            else
+                M[j].add(term1.add(M[j - 1][i - 1]))
+        }
+    }
+
+    return M
+}
+
--- src/test/kotlin/com/example/major/MajorApplicationTests.kt
+++ src/test/kotlin/com/example/major/MajorApplicationTests.kt
@@ -2,6 +2,7 @@ package com.example.major
 
 import com.example.major.NetworkData.AuthorNetworkNode
 import org.junit.jupiter.api.Test
+import org.nd4j.linalg.factory.Nd4j
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -25,7 +26,27 @@ class MajorApplicationTests {
 			AuthorNetworkNode("i", "i", listOf())
 		)
 
+		var Q = Nd4j.zeros(2, 2)
+		Q.put(0, 0, 1)
+		println(Q)
+	}
 
+	@Test
+	fun getMatrixM() {
+		var G: List<AuthorNetworkNode> = listOf(
+			AuthorNetworkNode("a", "a", listOf("b", "d", "e")) ,
+			AuthorNetworkNode("b", "b", listOf("c", "f", "g", "i")),
+			AuthorNetworkNode("c", "c", listOf()),
+			AuthorNetworkNode("d", "d", listOf("c", "g", "i")),
+			AuthorNetworkNode("e", "e", listOf("h", "i")),
+			AuthorNetworkNode("f", "f", listOf("d")),
+			AuthorNetworkNode("g", "g", listOf()),
+			AuthorNetworkNode("h", "h", listOf()),
+			AuthorNetworkNode("i", "i", listOf())
+		)
+
+		var M = com.example.major.SNAConcept.getMatrixM(0.6, 4, 3, G)
+		println(M[1][4])
 	}
 
 }
