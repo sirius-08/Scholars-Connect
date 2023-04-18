f145018ac7fdd151ded1e4b4409b19b27d433b47
sirius-08 <kalra.tanish8@gmail.com>
2023-04-19 01:22:16 +0530

    Completed OpeningPosting
    Added the getApplicants and the getAvailableOpenings

--- src/main/kotlin/com/example/major/OpeningsPosting/OpeningsRepository.kt
+++ src/main/kotlin/com/example/major/OpeningsPosting/OpeningsRepository.kt
@@ -6,4 +6,6 @@ import java.util.Optional
 interface OpeningsRepository: MongoRepository<Opening, String> {
     fun findAllByAuthorId(authorId: String): List<Opening>
     fun findOpeningById(id: String): Optional<Opening>
+    fun findAllByAuthorIdNotLike(authorId: String): List<Opening>
+    fun findOpeningsByAuthorIdNotLike(authorId: String): List<Opening>
 }
--- src/main/kotlin/com/example/major/OpeningsPosting/OpeningsService.kt
+++ src/main/kotlin/com/example/major/OpeningsPosting/OpeningsService.kt
@@ -1,13 +1,17 @@
 package com.example.major.OpeningsPosting
 
+import com.example.major.ContentPosting.Post
 import com.example.major.RegisteredUsers.RegistrationService
+import com.example.major.RegisteredUsers.User
+import com.example.major.SNAConcept.SNAService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import org.springframework.stereotype.Service
 import java.util.UUID
+import kotlin.math.abs
 
 @Service
-class OpeningsService @Autowired constructor(var openingsRepository: OpeningsRepository,var registrationService: RegistrationService){
+class OpeningsService @Autowired constructor(var openingsRepository: OpeningsRepository,var registrationService: RegistrationService, var snaService: SNAService){
     fun postOpening(opening: Opening) {
         opening.id = UUID.randomUUID().toString()
         opening.timeOfPosting = System.currentTimeMillis()
@@ -28,5 +32,42 @@ class OpeningsService @Autowired constructor(var openingsRepository: OpeningsRep
             opening.get().applicants.add(registrationService.getUserById(userId))
             openingsRepository.save(opening.get())
         }
+    }
+
+    fun getAvailableOpenings(userId: String): List<Opening> {
+        var openings:MutableList<Opening> = openingsRepository.findOpeningsByAuthorIdNotLike(userId).toMutableList()
+        val idToSimRank: HashMap<String, Double> = hashMapOf()
+        val idToKCore: HashMap<String, Int> = hashMapOf()
+
+        val userKCore = registrationService.getKCoreForUser(userId)
+
+        openings.forEach { it ->
+            if(!idToSimRank.containsKey(it.authorId)) {
+                println(userId)
+                println(it.authorId)
+                idToSimRank[it.authorId] = snaService.getSimRankScoreForTwoUsers(userId, it.authorId)
+                idToKCore[it.authorId] = abs(userKCore - registrationService.getKCoreForUser(it.authorId))
+            }
+        }
+
+        return openings.sortedWith(compareBy({idToKCore[it.authorId]}, {1000000.0 - idToSimRank[it.authorId]!!}))
+    }
+
+    fun getApplicants(id: String, userId: String): List<User> {
+        var applicants = openingsRepository.findOpeningById(id).get().applicants.toMutableList()
+
+        var idToSimRank: HashMap<String, Double> = hashMapOf()
+        var idToKCore: HashMap<String, Int> = hashMapOf()
+
+        var userKCore = registrationService.getKCoreForUser(userId)
+
+        applicants.forEach { it ->
+            if(!idToSimRank.containsKey(it.id)) {
+                idToSimRank[it.id] = snaService.getSimRankScoreForTwoUsers(userId, it.id)
+                idToKCore[it.id] = abs(userKCore - registrationService.getKCoreForUser(it.id))
+            }
+        }
+
+        return applicants.sortedWith(compareBy({idToKCore[it.id]}, {1000000.0 - idToSimRank[it.id]!!}))
     }
 }
